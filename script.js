a773a57144ebf4da9d66e7d4e83f6ae7eec60eaf
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-03-31 07:42:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function showAlert() {
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const phoneNumber = document.getElementById("phone-number").value;
+  const emailId = document.getElementById("email-id").value;
+
+  alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nPhone Number: " + phoneNumber + "\nEmail ID: " + emailId);
+}
